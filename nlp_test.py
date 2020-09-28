1268eb71c4adc3f410955f0d1b77a19cd9e09046
nina.prabhu <nina.prabhu@zanbato.com>
2020-09-28 15:00:38 -0700

    Split data, clean up

--- nlp_test.py
+++ nlp_test.py
@@ -2,13 +2,9 @@ from nltk.corpus import twitter_samples, stopwords #data
 from nltk.tag import pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk import FreqDist
-
 import re, string
 
-# positive_tweets = twitter_samples.strings('positive_tweets.json') #5000
-# negative_tweets = twitter_samples.strings('negative_tweets.json') #5000
 text = twitter_samples.strings('tweets.20150430-223406.json') #20000 - unknown sentiment
-# tweet_tokens = twitter_samples.tokenized('positive_tweets.json')[0]
 stop_words = stopwords.words('english')
 
 #normalize words to infinitive/singular - run, running, ran
@@ -39,6 +35,25 @@ def get_all_words(cleaned_tokens_list):
         for token in tokens:
             yield token
 
+def get_tweets_for_model(cleaned_tokens_list):
+    for tweet_tokens in cleaned_tokens_list:
+        yield dict([token, True] for token in tweet_tokens)
+
+def make_dataset(positive_tokens_for_model, negative_tokens_for_model):
+    positive_dataset = [(tweet_dict, "Positive")
+                         for tweet_dict in positive_tokens_for_model]
+
+    negative_dataset = [(tweet_dict, "Negative")
+                         for tweet_dict in negative_tokens_for_model]
+
+    dataset = positive_dataset + negative_dataset
+
+    random.shuffle(dataset)
+
+    train_data = dataset[:7000]
+    test_data = dataset[7000:]
+    return train_data, test_data
+
 positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
 negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
 
@@ -51,13 +66,10 @@ for tokens in positive_tweet_tokens:
 for tokens in negative_tweet_tokens:
     negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
-all_pos_words = get_all_words(positive_cleaned_tokens_list)
-all_neg_words = get_all_words(negative_cleaned_tokens_list)
+positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
+negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
 
-freq_dist_pos = FreqDist(all_pos_words)
-freq_dist_neg = FreqDist(all_neg_words)
-
-print(freq_dist_pos.most_common(10))
+train_data, test_data = make_dataset(positive_tokens_for_model, negative_tokens_for_model)
 
 
 
